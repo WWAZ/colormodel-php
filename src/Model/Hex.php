99effae7b5c51b96b9523e8a2604ce4c866c3ece
WWAZ <azwierzynski@gmx.de>
2025-05-20 13:16:17 +0200

    refactoring

--- src/Model/Hex.php
+++ src/Model/Hex.php
@@ -66,14 +66,6 @@ class Hex extends Color
      */
     public function toRGB()
     {
-        // // $hexString = ltrim($this->hex, '#'); // "#" remove, when existing
-        // // $hex       = hexdec($hexString);     // z. B. "FF00FF" -> 16711935
-        // $hex = $this->hex;
-
-        // $red   = (($hex & 0xFF0000) >> 16);
-        // $green = (($hex & 0x00FF00) >> 8);
-        // $blue  = (($hex & 0x0000FF));
-        // return new RGB($red, $green, $blue);
         $hex = $this->hex;
         
         if( is_string($hex) ){
@@ -83,7 +75,7 @@ class Hex extends Color
         $red   = (($hex & 0xFF0000) >> 16);
         $green = (($hex & 0x00FF00) >> 8);
         $blue  = (($hex & 0x0000FF));
-        
+
         return new RGB($red, $green, $blue);
     }
 
