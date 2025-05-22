a008ffd8322751171cc95c1115caee88a52d4488
WWAZ <azwierzynski@gmx.de>
2025-05-22 18:22:22 +0200

    schemes added

--- src/Scheme/Analogous.php
+++ src/Scheme/Analogous.php
@@ -9,8 +9,8 @@ class Analogous extends Scheme
     public function get()
     {
         return [
-            clone $this->color->hue(-30),
             clone $this->color,
+            clone $this->color->hue(-30),
             clone $this->color->hue(30),
         ];
     }
